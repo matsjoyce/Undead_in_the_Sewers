6ad827761d73223433248a7f7e501142c887e24f
Matthew Joyce <matsjoyce@gmail.com>
2023-04-16 20:54:45 +0100

    Back at it! Fixes for 1.16

--- lua/wml_tags.lua
+++ lua/wml_tags.lua
@@ -1,18 +1,18 @@
 function wesnoth.wml_actions.store_shroud(args)
-    local team_num = args.side or helper.wml_error("[store_shroud] expects a side= attribute.")
-    local storage = args.variable or helper.wml_error("[store_shroud] expects a variable= attribute.")
-    local team = wesnoth.get_side(team_num)
+    local team_num = args.side or wml.error("[store_shroud] expects a side= attribute.")
+    local storage = args.variable or wml.error("[store_shroud] expects a variable= attribute.")
+    local team = wesnoth.sides[team_num]
     local current_shroud = team.__cfg.shroud_data
     wesnoth.set_variable(storage,current_shroud)
 end
 
 function wesnoth.wml_actions.set_shroud(args)
-    local team_num = args.side or helper.wml_error("[storeet_shroud] expects a side= attribute.")
-    local shroud = args.shroud_data or helper.wml_error("[set_shroud] expects a shroud_data= attribute.")
+    local team_num = args.side or wml.error("[set_shroud] expects a side= attribute.")
+    local shroud = args.shroud_data or wml.error("[set_shroud] expects a shroud_data= attribute.")
     if shroud == nil then
-        helper.wml_error("[set_shroud] was passed a nil shroud string")
+        wml.error("[set_shroud] was passed a nil shroud string")
     elseif string.sub(shroud,1,1)~="|" then
-        helper.wml_error("[set_shroud] was passed an invalid shroud string.")
+        wml.error("[set_shroud] was passed an invalid shroud string.")
     else
         local w,h,b=wesnoth.get_map_size()
         local shroud_x= (1-b)
@@ -20,7 +20,7 @@ function wesnoth.wml_actions.set_shroud(args)
             local shroud_y=(1-b)
             for c in string.gmatch(r,"%d") do
                 if c == "1" then
-                    wesnoth.fire("remove_shroud",{side=team_num,x=shroud_x,y=shroud_y})
+                    wml.fire("remove_shroud",{side=team_num,x=shroud_x,y=shroud_y})
                 end
                 shroud_y=shroud_y+1
             end
