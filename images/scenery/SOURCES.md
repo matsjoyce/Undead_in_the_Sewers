dc3ed42dcc39609b42efa27e439cf7e5d39441fa
Matthew Joyce <matsjoyce@gmail.com>
2023-05-10 19:26:17 +0100

    Start on Delfador boss fight

---
+++ images/scenery/SOURCES.md
@@ -0,0 +1,1 @@
+Mirror art came from the Mountain Witch addon from 1.12
--- lua/wml_tags.lua
+++ lua/wml_tags.lua
@@ -1,18 +1,18 @@
 function wesnoth.wml_actions.store_shroud(args)
-    local team_num = args.side or wml.error("[store_shroud] expects a side= attribute.")
-    local storage = args.variable or wml.error("[store_shroud] expects a variable= attribute.")
+    local team_num = args.side or wml.error("[store_shroud] expects a side= attribute")
+    local storage = args.variable or wml.error("[store_shroud] expects a variable= attribute")
     local team = wesnoth.sides[team_num]
     local current_shroud = team.__cfg.shroud_data
     wesnoth.set_variable(storage,current_shroud)
 end
 
 function wesnoth.wml_actions.set_shroud(args)
-    local team_num = args.side or wml.error("[set_shroud] expects a side= attribute.")
-    local shroud = args.shroud_data or wml.error("[set_shroud] expects a shroud_data= attribute.")
+    local team_num = args.side or wml.error("[set_shroud] expects a side= attribute")
+    local shroud = args.shroud_data or wml.error("[set_shroud] expects a shroud_data= attribute")
     if shroud == nil then
         wml.error("[set_shroud] was passed a nil shroud string")
     elseif string.sub(shroud,1,1)~="|" then
-        wml.error("[set_shroud] was passed an invalid shroud string.")
+        wml.error("[set_shroud] was passed an invalid shroud string")
     else
         local w,h,b=wesnoth.get_map_size()
         local shroud_x= (1-b)
